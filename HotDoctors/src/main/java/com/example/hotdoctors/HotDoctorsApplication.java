36eba59412ae5f4883b3a0c7baec393303f44931
22eragon22 <maciejstawiarz1@gmail.com>
2022-03-12 18:56:07 +0100

    Just some basic dependencies and basic files

---
+++ HotDoctors/src/main/java/com/example/hotdoctors/HotDoctorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotdoctors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotDoctorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotDoctorsApplication.class, args);
+    }
+
+}
---
+++ HotDoctors/src/test/java/com/example/hotdoctors/HotDoctorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotdoctors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotDoctorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
