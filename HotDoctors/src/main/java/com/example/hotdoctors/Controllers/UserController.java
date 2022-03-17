06af2a815ace8204135bb0c901283dbf2cb8ebb6
Maciek <Your_Majesty@wp.pl>
2022-03-17 10:03:37 +0100

    declaring visitService in controller

--- HotDoctors/src/main/java/com/example/hotdoctors/Controllers/UserController.java
+++ HotDoctors/src/main/java/com/example/hotdoctors/Controllers/UserController.java
@@ -4,8 +4,8 @@ package com.example.hotdoctors.Controllers;
 import com.example.hotdoctors.Comment.CommentService;
 import com.example.hotdoctors.Schedule.ScheduleService;
 import com.example.hotdoctors.User.UserService;
+import com.example.hotdoctors.Visit.VisitService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +15,6 @@ public class UserController {
     private final UserService userService;
     private final ScheduleService scheduleService;
     private final CommentService commentService;
+    private final VisitService visitService;
 
 }
