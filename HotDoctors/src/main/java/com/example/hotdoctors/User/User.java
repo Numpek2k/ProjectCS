e7bc748b68b25277b1d5a905d30f6baae3106085
ulpioro <78817508+ulpioro@users.noreply.github.com>
2022-03-22 13:25:29 +0100

    Added login and register component with html

--- HotDoctors/src/main/java/com/example/hotdoctors/User/User.java
+++ HotDoctors/src/main/java/com/example/hotdoctors/User/User.java
@@ -35,33 +35,5 @@ public class User {
     private String description;
     private String address;
 
-    public User(String name, String surname, String email, String password, UserType role, String image, String description, String address) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.image = image;
-        this.description = description;
-        this.address = address;
-    }
 
-
-    public void setId(int id) { this.id = id; }
-
-    public void setName(String name) { this.name = name; }
-
-    public void setSurname(String surname) { this.surname = surname; }
-
-    public void setEmail(String email) { this.email = email; }
-
-    public void setPassword(String password) { this.password = password; }
-
-    public void setRole(UserType role) { this.role = role; }
-
-    public void setImage(String image) { this.image = image; }
-
-    public void setDescription(String description) { this.description = description; }
-
-    public void setAddress(String address) { this.address = address; }
 }
