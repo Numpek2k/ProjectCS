b52db8e1bad51b09b019eee34c110aba05b5d9dd
ulpioro <78817508+ulpioro@users.noreply.github.com>
2022-05-24 13:48:48 +0200

    deleted useless comments

--- LekarioFront/src/app/components/home/home.component.ts
+++ LekarioFront/src/app/components/home/home.component.ts
@@ -12,18 +12,6 @@ export class HomeComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    // this.userService.login('sjsgdf@o2.pl', 'asuydfu');
-    // this.userService.getAllUsers().subscribe(res => console.log(res));
-    // this.userService.refresh();
-    // let user: User = {
-    //   name: 'jdsafgb',
-    //   surname: 'asfdsdf',
-    //   email: 'sjsgdf@o2.pl',
-    //   password: 'asuydfu'
-    // }
-    // this.userService.register(user);
-    // this.userService.getUserById(72).subscribe(console.log);
-    //   this.userService.getUserByEmail('asdhfosa@o2.pl').subscribe(console.log);
   }
 
 
